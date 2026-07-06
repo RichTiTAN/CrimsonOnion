8ccec494040a9852fa687522c36edfb35635ceb6
RichTiTAN <137271949+RichTiTAN@users.noreply.github.com>
2026-07-06 14:12:33 +0330

    Updated to v2.0.1

--- Services/EngineConfigWriters.cs
+++ Services/EngineConfigWriters.cs
@@ -1,3 +1,21 @@
+/*
+ * CrimsonOnion - A GUI client that runs multiple Tor instances and load-balances them.
+ * Copyright (C) 2026 RichTiTAN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
